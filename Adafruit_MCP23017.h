34cf7233ff6480a48b3a902879da2509f972a78d
TribesTom <tribes.tom@gmail.com>
2015-01-16 14:02:23 +0100

    Cosa update
    Library change for COSA

--- Adafruit_MCP23017.h
+++ Adafruit_MCP23017.h
@@ -9,47 +9,19 @@
 
   Written by Limor Fried/Ladyada for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
- ****************************************************/
-
+***************************************************
+ Updated by TribesTom
+ 
+ Changed tu support COSA arduino libraries.
+ 
+ 
+ 
+ */
+ 
 #ifndef _Adafruit_MCP23017_H_
 #define _Adafruit_MCP23017_H_
 
-// Don't forget the Wire library
-class Adafruit_MCP23017 {
-public:
-  void begin(uint8_t addr);
-  void begin(void);
-
-  void pinMode(uint8_t p, uint8_t d);
-  void digitalWrite(uint8_t p, uint8_t d);
-  void pullUp(uint8_t p, uint8_t d);
-  uint8_t digitalRead(uint8_t p);
-
-  void writeGPIOAB(uint16_t);
-  uint16_t readGPIOAB();
-  uint8_t readGPIO(uint8_t b);
-
-  void setupInterrupts(uint8_t mirroring, uint8_t open, uint8_t polarity);
-  void setupInterruptPin(uint8_t p, uint8_t mode);
-  uint8_t getLastInterruptPin();
-  uint8_t getLastInterruptPinValue();
-
- private:
-  uint8_t i2caddr;
-
-  uint8_t bitForPin(uint8_t pin);
-  uint8_t regForPin(uint8_t pin, uint8_t portAaddr, uint8_t portBaddr);
-
-  uint8_t readRegister(uint8_t addr);
-  void writeRegister(uint8_t addr, uint8_t value);
-
-  /**
-   * Utility private method to update a register associated with a pin (whether port A/B)
-   * reads its value, updates the particular bit, and writes its value.
-   */
-  void updateRegisterBit(uint8_t p, uint8_t pValue, uint8_t portAaddr, uint8_t portBaddr);
-
-};
+#include "Cosa/TWI.hh"
 
 #define MCP23017_ADDRESS 0x20
 
@@ -80,5 +52,67 @@ public:
 #define MCP23017_OLATB 0x15
 
 #define MCP23017_INT_ERR 255
+
+#define INPUT 1
+#define OUTPUT 0
+
+#define CHANGE 0
+
+#define FALLING 1
+#define RISING 0
+
+#define HIGH 1
+#define LOW 0
+
+
+class MCP23017 : private TWI::Driver {
+public:
+  MCP23017(uint8_t addr) :
+  TWI::Driver(MCP23017_ADDRESS| (addr & 0x7))
+  {}
+  void begin();
+  void pinMode(uint8_t p, uint8_t d);
+  void digitalWrite(uint8_t p, uint8_t d);
+  void pullUp(uint8_t p, uint8_t d);
+  uint8_t digitalRead(uint8_t p);
+
+  void writeGPIOAB(uint16_t);
+  uint16_t readGPIOAB();
+  uint8_t readGPIO(uint8_t b);
+
+  void setupInterrupts(uint8_t mirroring, uint8_t open, uint8_t polarity);
+  void setupInterruptPin(uint8_t p, uint8_t mode);
+  uint8_t getLastInterruptPin();
+  uint8_t getLastInterruptPinValue();
+
+protected:
+/** Pin number mask. */
+  static const uint8_t PIN_MASK = 0x07;
+
+/* Construct connection to MCP23017 Remote 16-bit I/O expander with
+* given address.
+* @param[in] addr bus address.
+* @param[in] subaddr device sub address.
+*/
+  MCP23017(uint8_t addr, uint8_t subaddr) :
+  TWI::Driver(addr | (subaddr & 0x7))
+  {}
+
+private:
+  uint8_t bitForPin(uint8_t pin);
+  uint8_t regForPin(uint8_t pin, uint8_t portAaddr, uint8_t portBaddr);
+
+  uint8_t readRegister(uint8_t addr);
+  void writeRegister(uint8_t addr, uint8_t value);
+
+  /**
+   * Utility private method to update a register associated with a pin (whether port A/B)
+   * reads its value, updates the particular bit, and writes its value.
+   */
+  void updateRegisterBit(uint8_t p, uint8_t pValue, uint8_t portAaddr, uint8_t portBaddr);
+
+};
+
+
 
 #endif
